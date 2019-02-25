a6a391b74f6b7dff0db7959734bcb9a87d3fbd99
Lefevre <lucie.lefevre@ensta-bretagne.org>
2019-02-25 09:03:21 +0100

    ajout de la structure du robot et de roblib

---
+++ Filtre_particulaire/Python/script/particulaire_python.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 21 10:19:09 2019
+
+@author: Lucie
+"""
+
+import roblib 
+import numpy as np
+
+
+
+
+class Particule:
+    def __init__(self):
+        self.x = np.zeros((3,1))
+        self.Mco = np.zeros((3,3))
+        
+    def __repr__(self):
+        return "Vecteur etat :[0},{},{}]".format(self.x[0],self.x[1],self.x[2]) + "\n Matrice de covariance : {}".format(self.Mco)  # {:.2f} notation pour n'afficher que deux chiffres après la virgule
+        
+    def controle(self):
+        return 0, 0
+    
+    def f(x,theta,u):
+        """ equation d'etat de l'AUV"""
+        x = x.flatten()
+        A  = np.array([[0,0,np.cos(theta)],[0,0,np.sin(theta)],[0,0,-1]])
+        return A @ x + np.array([[0,0,u]]).T
+
+
+        
+        
+       
+    
+   
