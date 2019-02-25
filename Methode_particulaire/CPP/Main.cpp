09dc402449af90b7646476b5f6d67a55ec3f985f
phoenix1249 <maxime.do@ensta-bretagne.org>
2019-02-25 14:30:02 +0100

    Modification Main

--- Methode_particulaire/CPP/Main.cpp
+++ Methode_particulaire/CPP/Main.cpp
@@ -5,14 +5,17 @@ using namespace std;
 int main(int argc, char **argv){
   Gnuplot gp;
 
-  for (j=1,j<=1000,j+=1){
-    for (i=1; i<=100; i+=1){
-      Robot robot;
-      Robot.scenario();
-      Robot.draw(gp);
-
-    }
+  vector<Robot> List_robot;
+  for (int i=0; i<100;i++){
+    List_robot.push_back(Robot())
   }
 
+  for (int j=1,j<=1000,j++1){
+    for (int i=1; i<100; i++1){
+      robot = List_robot.at<Robot>(i)
 
+      robot.scenario();
+      robot.draw(gp);
+    }
+  }
 }
