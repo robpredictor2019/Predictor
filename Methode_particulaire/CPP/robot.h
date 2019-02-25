fc33c96fa0bf1a9b58b40ed55a6f17200ab8ca31
brugieju <juliette.brugier@ensta-bretagne.org>
2019-02-25 09:45:04 +0100

    header robot

--- Methode_particulaire/CPP/robot.h
+++ Methode_particulaire/CPP/robot.h
@@ -6,9 +6,22 @@ private:
   float m_theta;
   float m_u;
 
-  //Methodes
 public:
-  void evolution(Robot);
-  void draw(Robot);
+  Robot(); // Constructeur
+  void Show() const; // Affichage
+  //Methodes
+  void evolution();
+  void draw();
+  float scenario();
+  //setter
+  void setX(float x);
+  void setY(float y);
+  void setTheta(float theta);
+  void setU(float u);
+  //getter
+  int getX() const;
+  int getY() const;
+  int getTheta() const;
+  int getU() const;
 
 };
