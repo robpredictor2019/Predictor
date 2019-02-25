bec165938013d7be18cdc2833e72c7029f266aba
victor <victor.bares@ensta-bretagne.org>
2019-02-25 15:25:37 +0100

    ajout des modifications du main

--- Methode_particulaire/CPP/Main.cpp
+++ Methode_particulaire/CPP/Main.cpp
@@ -1,24 +1,36 @@
 #include "robot.h"
 #include "iostream"
 using namespace std;
+#define NOMBREROBOT=100
+#define NOMBREITERATION=1000
 
 int main(int argc, char **argv){
   Robot robot
   Gnuplot gp;
-
   vector<Robot> List_robot;
+
+  std::fstream fs;
+  fs.open ("Robot_States.txt", std::fstream::in | std::fstream::out | std::fstream::app);
+
   for (int i=0; i<100;i++){
     List_robot.push_back(Robot())
   }
 
 
-  for (int j=1,j<=1000,j++1){
-    for (int i=1; i<100; i++1){
+  for (int j=0,j<=nombreIteration,j++){
+    for (int i=0; i<nombreRobot; i++){
       robot = List_robot.at<Robot>(i)
-
-      robot.scenario();
+      robot.evolution();
       robot.draw(gp);
+      robot.save();
     }
   }
-}
+  for(int i=0; i<nombreRobot;i++){
+    robot = List_robot.at<Robot>(i)
+    robot.export(fs)
 
+  }
+  fs.close()
+
+  return 0;
+}
