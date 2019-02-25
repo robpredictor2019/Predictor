3da040869b64d79f70725025ec020cae89415c94
clavier <evann.clavier@ensta-bretagne.org>
2019-02-25 11:33:36 +0100

    maj robot

--- Methode_particulaire/CPP/robot.h
+++ Methode_particulaire/CPP/robot.h
@@ -1,8 +1,6 @@
 #include "Kalman.h"
-#include <vector>
-#include "gnuplot-iostream.h"
+#include <opencv2/core/core.hpp>
 
-typedef std::pair<double, double> Point;
 
 class Robot
 {
@@ -24,6 +22,9 @@ private:
   void evolution();
   void draw(Gnuplot gp);
   float scenario();
+  void kalman_predict(Mat xup_k,Mat Pup_k, Mat Q, Mat A, Mat u, Mat* x_k1, Mat* P_k1);
+  void kalman_correct(Mat x_k1, Mat P_k1, Mat  C, Mat R, Mat y, Mat* xup_k1, Mat* Pup_k1);
+  void kalman_x(Mat x_k,Mat P_k,Mat u,Mat y,Mat Q, Mat R,Mat A ,Mat C, Mat* P_k1, Mat* x_k1);
   //setter
   void setX(float x);
   void setY(float y);
@@ -34,4 +35,34 @@ private:
   int getY() const;
   int getTheta() const;
   int getU() const;
+  //variables kalman
+  double dt;
+  double h0;
+  double d0;
+  Mat x;
+  Mat u;
+  Mat phat;
+  Mat C;
+  Mat dx;
+  Mat A;
+  Mat theta_p;
+  Mat theta_beta;
+  Mat theta_alpha;
+  Mat y;
+  Mat phat_out, theta_p_out;
+  double hm;
+  bool depth_new;
+  bool alt_new;
+  int size_x;
+  int size_u;
+  int size_y;
+
+  double cap_command;
+  int count_command_test;
+  double count_mag;
+  double command_test ;
+
+  double m_hm[1] ;
+  Mat hm_mat;
+
 };
