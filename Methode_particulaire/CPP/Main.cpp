d70e7d4dc6c4d4e214654fadca09f9632c14080d
clavier <evann.clavier@ensta-bretagne.org>
2019-02-25 10:35:15 +0100

    add main and robot.cpp

--- Methode_particulaire/CPP/Main.cpp
+++ Methode_particulaire/CPP/Main.cpp
@@ -3,16 +3,6 @@
 using namespace std;
 
 int main(int argc, char **argv){
-  Gnuplot gp;
-
-  for (j=1,j<=1000,j+=1){
-    for (i=1; i<=100; i+=1){
-      Robot robot;
-      Robot.scenario();
-      Robot.draw(gp);
-
-    }
-  }
-
-
+  Robot robot;
+  Robot.scenario();
 }
