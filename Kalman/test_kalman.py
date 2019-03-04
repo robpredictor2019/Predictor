3001f4389d82ba10c7cfaeafdb8067e44b86cca1
olaurendin <olivier.laurendin@ensta-bretagne.org>
2019-03-04 18:48:41 +0100

    reglage bug matrice evolution

--- Kalman/test_kalman.py
+++ Kalman/test_kalman.py
@@ -12,7 +12,6 @@ np.set_printoptions(formatter={'float_kind':float_formatter})
 plt.close()
 
 class kalman_localisation:
-    epsilon = 1.e-4
 
     def __init__(self, X, Γα, u, r, dt):
         self.X = X
@@ -25,13 +24,13 @@ class kalman_localisation:
 
         # GPS
         self.C1 = np.array([[1., 0., 0.], [0., 1., 0.]])
-        self.Γβ1 = err_GPS*np.eye(2)
+        self.Γβ1 = (err_GPS**2)*np.eye(2)
         # DVL
         self.C2 = np.array([0., 0., 1.])
-        self.Γβ2 = err_DVL*np.eye(1)
+        self.Γβ2 = (err_DVL**2)*np.eye(1)
         # GPS + DVL
         self.C3 = np.eye(3)
-        self.Γβ3 = np.array([[err_GPS, 0., 0.], [0., err_GPS, 0.], [0., 0., err_DVL]])
+        self.Γβ3 = np.array([[(err_GPS**2), 0., 0.], [0., (err_GPS)**2, 0.], [0., 0., (err_DVL)**2]])
 
 #        self.flg_predict = 0
 #        self.flg_GPS = 1
@@ -40,9 +39,11 @@ class kalman_localisation:
         self.dX = np.array([0., 0., 0.])
         self.r = r
         self.dt = dt
+        self.epsilon = 2*dt
 
     def set_mission(self, lm):
         self.lm = lm
+        self.len_lm = len(lm)
 
     def apply_kalman_predict(self):
         return kalman_predict(self.X, self.Gup, self.u, self.Γα, self.A)
@@ -61,7 +62,7 @@ class kalman_localisation:
 
     def get_V_pt(self, i):
         pt, r = np.array(self.lm[i][0]), self.lm[i][1]
-        pt_1, r_1 = np.array(self.lm[i+1][0]), self.lm[i+1][1]
+        pt_1, r_1 = np.array(self.lm[(i+1)%self.len_lm][0]), self.lm[(i+1)%self.len_lm][1]
         V = pt_1 - pt
         return V, pt, r, pt_1, r_1
 
@@ -69,30 +70,40 @@ class kalman_localisation:
         self.lX = []
         V, pt, r, pt_1, r_1 = self.get_V_pt(0)
         theta = sawtooth(arctan2(V[1], V[0]))
-        self.A = np.array([[0., 0., cos(theta)], [0., 0., sin(theta)], [0., 0., -1.]])
+        self.A = np.array([[1., 0., self.dt*cos(theta)],
+                           [0., 1., self.dt*sin(theta)],
+                           [0., 0., 1.-self.dt]])
         j = 0
+        flag = 0
+        i_old = 0
         for i in range(self.r):
-            if np.linalg.norm(self.X[:2] - pt_1) < self.epsilon :    # Nouvelle île détectée
+            if flag == 1 :    # Nouvelle île détectée
+                flag =0
                 j += 1
+                i_old = i
+                if j == self.len_lm:
+                    break
                 V, pt, r, pt_1, r_1 = self.get_V_pt(j)
                 theta = arctan2(V[1], V[0])
-                self.A = np.array([[0., 0., cos(theta)], [0., 0., sin(theta)], [0., 0., -1.]])
+                self.A = np.array([[1., 0., self.dt*cos(theta)],
+                                   [0., 1., self.dt*sin(theta)],
+                                   [0., 0., 1.-self.dt]])
             d = self.dist_min_amers(pt, r, pt_1, r_1)
-            if d>0:         # UAV proche d'un point, remonte à la surface, passage en GPS
+            d =  (i-i_old) - np.linalg.norm(pt_1-pt)/self.dt
+            if d>=0:         # UAV proche d'un point, remonte à la surface, passage en GPS
 #               print("full\n")
-                self.dX, self.Gup = self.apply_kalman_GPS()
+                self.X, self.Gup = self.apply_kalman_GPS()
+                flag =1
             else:         # UAV en haute mer, plonge, passage en kalman predictif
 #               print("DVL\n")
-                self.dX, self.Gup = self.apply_kalman_predict()
-            for i in range(3):
-                self.X[i] += self.dt*self.dX[i]
+                self.X, self.Gup = self.apply_kalman_predict()
             self.lX.append(np.array([self.X[0], self.X[1], theta]))
             try :
 #               print("try")
-                self.lGup = np.dstack((self.lGup, np.sqrt(self.Gup)))
+                self.lGup = np.dstack((self.lGup, np.sqrt(abs(self.Gup))))
             except AttributeError :
 #               print("except")
-                self.lGup = np.sqrt(self.Gup)
+                self.lGup = np.sqrt(abs(self.Gup))
         self.plot_results()
 
     def dist_min_amers(self, pt, r, pt_1, r_1):
@@ -101,14 +112,16 @@ class kalman_localisation:
     def plot_results(self):
         fig = plt.figure()
         ax = fig.add_subplot(111, aspect='equal')
-        ax.set_xlim(-5,15)
-        ax.set_ylim(-5,15)
+        ax.set_xlim(-3,12)
+        ax.set_ylim(-3,8)
         for i in range(len(self.lX)):
-            draw_tank(self.lX[i],col='darkblue',r=0.1)
-
-            if i%100 == 0 :
+            # draw_tank(self.lX[i],col='darkblue',r=0.1)
+            if i%20 == 0 :
                     draw_ellipse(self.lX[i][:2].T,self.lGup[0,0,i], self.lGup[1,1,i],ax,"blue")
             # draw_ellipse(self.lX[i].T,self.lGup[0,0], self.lGup[1,1],ax,"blue")
+        nlX = np.array(self.lX)
+        plt.plot(nlX[:,0],nlX[:,1])
+        # print(self.lX[:][0][:][0])
         for i in range(3):
             plt.figure()
             plt.plot(self.lGup[i,i])
@@ -116,7 +129,7 @@ class kalman_localisation:
 
 if __name__ == "__main__":
     X = np.array([0., 0., 1.])
-    Γα = 1.*np.eye(3)
+    Γα = 0.01*np.eye(3)
     u = np.array([0., 0., 1.])
     r = 2500
     dt = 0.01
@@ -124,7 +137,7 @@ if __name__ == "__main__":
     # ax=init_figure(-1,6,-1,6)
 
     kl = kalman_localisation(X, Γα, u, r, dt)
-    lm = [[[0.,0.], 1.], [[10.,1.], 1.], [[5.,5.], 1.], [[0.,0.], 1.]]
+    lm = [[[0.,0.], 1.], [[10.,1.], 1.], [[5.,5.], 1.]]
     kl.set_mission(lm)
     kl.loop()
     plt.show()
