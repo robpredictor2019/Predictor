9875ba957744fe881602dd0724cd0efaa07eedd0
Lefevre <lucie.lefevre@ensta-bretagne.org>
2019-03-06 09:45:14 +0100

    ajout mission triangle

--- Methode_particulaire/Python/include/particule.py
+++ Methode_particulaire/Python/include/particule.py
@@ -12,20 +12,20 @@ from PyUnityVibes.UnityFigure import UnityFigure
 
 def kalman_predict(xup,Gup,u,Γα,A):
     Γ1 = A @ Gup @ A.T + Γα
-    x1 = A @ xup + u 
-    return(x1,Γ1)    
+    x1 = A @ xup + u
+    return(x1,Γ1)
 
 def kalman_correc(x0,Γ0,y,Γβ,C):
-    S = C @ Γ0 @ C.T + Γβ 
+    S = C @ Γ0 @ C.T + Γβ
     if det(S) != 0 :
         K = Γ0 @ C.T @ inv(S)
-    else : 
+    else :
         K = zeros((3,2))
-    ytilde = y - C @ x0        
-    Gup = (eye(len(x0))-K @ C) @ Γ0 
-    xup = x0+ K@ytilde    
-    return(xup,Gup) 
-    
+    ytilde = y - C @ x0
+    Gup = (eye(len(x0))-K @ C) @ Γ0
+    xup = x0+ K@ytilde
+    return(xup,Gup)
+
 def kalman(x0,Γ0,u,y,Γα,Γβ,A,C):
     xup,Gup = kalman_correc(x0,Γ0,y,Γβ,C)
     x1,Γ1=kalman_predict(xup,Gup,u,Γα,A)
@@ -62,6 +62,7 @@ class Particule:
         self.theta = 0
         self.auv = figure.create(UnityFigure.OBJECT_3D_SUBMARINE, 0, 0, 0, dimX=5, dimY=5, dimZ=5, color=UnityFigure.COLOR_YELLOW)
         self.auv.updateRotation(0,math.degrees(self.U[1,0]),0)
+        self.omega_max = 10 * 2*np.pi/360
         time.sleep(0.1)
 
 
@@ -80,6 +81,9 @@ class Particule:
 
     def noise(self, variance):
         return np.random.normal(0,variance**2)
+
+    def distance_amer(self,amer):
+        return math.sqrt((amer[1]-self.X[1,0])**2 + (amer[0]-self.X[0,0])**2)
 
     def sign(self, a):
         if a > 0:
@@ -103,9 +107,9 @@ class Particule:
         """
         Control equation of the AUV
         """
-        k = 1
+
         #print(">>>", theta_target, self.theta)
-        self.U[1,0] = k*(theta_target-self.theta)
+        self.theta += 0.1 * (theta_target-self.theta) * min(self.omega_max,abs(theta_target-self.theta))
 
     def f(self):
         """
@@ -114,7 +118,7 @@ class Particule:
         alpha : bruit gaussien sur x,y et v
         """
         #print("u : ", self.U.flatten())
-        theta = self.U[1,0]
+
         u = self.U[0,0]
 
         sigma_x, sigma_y,sigma_v = 0.1,0.1,0.15
@@ -125,11 +129,11 @@ class Particule:
         alpha[1,0] = np.random.normal(0,sigma_y**2)
         alpha[2,0] = np.random.normal(0,sigma_v**2)
 
-        A  = array([[0,0,cos(theta)],[0,0,sin(theta)],[0,0,-1]])
+        A  = array([[0,0,cos(self.theta)],[0,0,sin(self.theta)],[0,0,-1]])
         return A.dot(self.X) + array([[0],[0],[u]]) + alpha
 
 
-    def step(self,t,dt):
+    def step_aller_retour(self,t,dt):
         if t == 60:
             C = array([[1,0,0],[0,1,0]])
             G_beta = diag([[0.45**2],[0.45**2]])
@@ -148,14 +152,35 @@ class Particule:
         #print("X avant : [{},{},{}]".format(self.X[0,0], self.X[1,0], self.X[2,0]))
         self.X = self.X + dt*self.f()
         #print("X apres : [{},{},{}]".format(self.X[0,0], self.X[1,0], self.X[2,0]))
-        
+
         U = self.U.flatten()
-        
+
         A  = array([[1,0,cos(self.theta)],[0,1,sin(self.theta)],[0,0,-1]])
         self.Xchap,self.cov = kalman(self.X,self.cov,array([[0],[0],[U[0]]]),G_beta ,G_alpha,G_beta,A,C)
         self.controle(t, theta_target)
 
+    def step_mission(self,t,dt,presence_amer,amer_target,theta_target):
 
+        if presence_amer == True:
+            C = array([[1,0,0],[0,1,0]])
+            G_beta = diag([[0.45**2],[0.45**2]])
+        else :
+            C = zeros((2,3))
+            G_beta = zeros((2,2))
+
+
+        sigma_x, sigma_y,sigma_v = 0.1,0.1,0.15
+        G_alpha = np.diag([sigma_x**2,sigma_y**2,sigma_v**2])
+
+        #print("X avant : [{},{},{}]".format(self.X[0,0], self.X[1,0], self.X[2,0]))
+        self.X = self.X + dt*self.f()
+        #print("X apres : [{},{},{}]".format(self.X[0,0], self.X[1,0], self.X[2,0]))
+
+        U = self.U.flatten()
+
+        A  = array([[1,0,cos(self.theta)],[0,1,sin(self.theta)],[0,0,-1]])
+        self.Xchap,self.cov = kalman(self.X,self.cov,array([[0],[0],[U[0]]]),G_beta ,G_alpha,G_beta,A,C)
+        self.controle(t, theta_target)
 
 
 
--- Methode_particulaire/Python/script/mission.py
+++ Methode_particulaire/Python/script/mission.py
@@ -10,21 +10,32 @@ class mission:
 	def __init__(self, num):
 		#Unity Display
 		self.figure = UnityFigure(UnityFigure.FIGURE_3D, UnityFigure.SCENE_EMPTY)
-		time.sleep(1)
+		time.sleep(2)
+
 
 		self.listParticules = [Particule(np.array([[0.0],[0.0],[0.0]]), np.array([[1.0],[0.0]]), np.diag((10**-9,10**-9,10**-9)), self.figure ) for i in range(num)]
+		time.sleep(1)
 		self.t = 0
 		self.dt = 0.1
 		self.tfinal = 30
 		self.num = num
-
 		self.anim = self.figure.createAnimation(self.dt)
+		self.listboue = [[50,0],[25,25],[0,0]]
+
 		time.sleep(1)
-	
+
 		print("Ajout des objets à l'animation")
 		for part in self.listParticules:
 			self.anim.addObject(part.auv)
-			time.sleep(1)
+			time.sleep(1.5)
+
+
+
+		for coord in self.listboue:
+			boue = self.figure.create(UnityFigure.OBJECT_3D_CUBE, coord[1], 0, coord[0], dimX=0.2, dimY=5, dimZ=0.2, color=UnityFigure.COLOR_BLACK)
+			time.sleep(1.5)
+			self.anim.addObject(boue)
+			time.sleep(1.5)
 
 	def __repr__(self):
 		return "Programme de la mission: \n Aller en ligne droite, retour a 60s\n Nombre de particules {}".format(self.num)
@@ -33,7 +44,7 @@ class mission:
 		print("Affichage de la mission sur PyUnityVibes")
 		self.figure.animate(self.anim)
 
-	def recalage(self):
+	def recalage(self,amer):
 		for part in self.listParticules:
 
 			x_gps = part.X[0,0] + part.noise(0.48)
@@ -43,11 +54,11 @@ class mission:
 			part.Xchap[1,0] = y_gps
 
 			part.cov = np.diag([0.48**2, 0.48**2, part.cov[2,2]])
-			
-			L = part.Xchap[0,0]
-			h = part.Xchap[1,0]
-			
-			part.theta = -(np.arctan2(h,L) + np.pi)
+
+			L = amer[0] - part.Xchap[0,0]
+			h = amer[1] - part.Xchap[1,0]
+
+			part.theta = np.arctan2(h,L) #-(np.arctan2(h,L) + np.pi)
 			part.U[1,0] = part.theta
 
 	def aller_retour(self):
@@ -58,12 +69,12 @@ class mission:
 		while self.t < self.tfinal :
 			#print("[{:.2f},{:.2f},{:.2f}]".format(self.listParticules[0].X[0,0], self.listParticules[0].X[1,0], self.listParticules[0].X[2,0]))
 			sys.stdout.write("Aller  : t = %f \r" % self.t)
-			for part in self.listParticules: 
-				part.step(self.t, self.dt)
+			for part in self.listParticules:
+				part.step_aller_retour(self.t, self.dt)
 				part.appendFrame(self.anim)
 			self.t  += self.dt
 
-		
+
 		print(self.listParticules[0])
 		self.recalage()
 		print(self.listParticules[0])
@@ -71,8 +82,8 @@ class mission:
 
 		while self.t < 2*self.tfinal:
 			#sys.stdout.write("Retour : t = %f \r" % self.t)
-			for part in self.listParticules: 
-				part.step(self.t, self.dt)
+			for part in self.listParticules:
+				part.step_aller_retour(self.t, self.dt)
 				part.appendFrame(self.anim)
 			self.t  += self.dt
 
@@ -81,10 +92,10 @@ class mission:
 		self.display()
 
 	def run(self):
-		while self.t < self.tfinal :	
+		while self.t < self.tfinal :
 			#sys.stdout.write("t = %f \r" % self.t)
-			for part in self.listParticules: 
-				part.step(self.t, self.dt)
+			for part in self.listParticules:
+				part.step_aller_retour(self.t, self.dt)
 				part.appendFrame(self.anim)
 			self.t  += self.dt
 
@@ -92,7 +103,34 @@ class mission:
 		time.sleep(1)
 		self.display()
 
+	def mission_triangle(self,liste_coord_amer):
+		T = 0
+		for amer in liste_coord_amer:
+			T += self.listParticules[0].distance_amer(amer)
+			print("T:",T)
+			print("t:",self.t)
+			print("amer :",amer)
+			presence_amer = True
+			theta_target = np.arctan2(amer[1] - self.listParticules[0].X[1,0],amer[0] - self.listParticules[0].X[0,0])
+			print("theta_target",theta_target)
+			#self.recalage(amer)
+			while self.t < T:
+				presence_amer = False
+				#print(amer,"[{:.2f},{:.2f},{:.2f},{:.2f}]".format(self.listParticules[0].X[0,0], self.listParticules[0].X[1,0], self.listParticules[0].X[2,0],self.listParticules[0].theta))#print("[{:.2f},{:.2f},{:.2f}]".format(self.listParticules[0].X[0,0], self.listParticules[0].X[1,0], self.listParticules[0].X[2,0]))
+				for part in self.listParticules:
+					#part.theta = np.arctan2(amer[1] - part.X[1,0],amer[0] - part.X[0,0])
+					part.step_mission(self.t, self.dt,presence_amer,amer,theta_target)
+					part.appendFrame(self.anim)
+
+				self.t += self.dt
+
+
+		print("\n Done ! ")
+		time.sleep(1)
+		self.display()
+
+
 if __name__=='__main__':
 	N = 10
 	mission = mission(N)
-	mission.aller_retour()
+	mission.mission_triangle([[50,0],[25,25],[0,0]])
